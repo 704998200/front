2b9853451a745d9716afd69309ea3150df7e40d0
Sparrow <704998200@qq.com>
2021-04-19 21:46:29 +0800

    3

---
+++ src/pages/Main.vue
@@ -0,0 +1,105 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="bg-grey-1">
+    <q-header elevated class="bg-white text-grey-8" height-hint="64">
+      <q-toolbar class="GNL__toolbar">
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+          class="q-mr-sm"
+        />
+
+        <q-toolbar-title v-if="$q.screen.gt.xs" shrink class="row items-center no-wrap">
+          <span>项目管理追踪系统</span>
+        </q-toolbar-title>
+
+        <div class="q-gutter-sm row items-center no-wrap">
+          <q-btn round flat>
+            <q-avatar size="26px" :class="Right">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <q-tooltip>Account</q-tooltip>
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-white"
+      :width="280"
+    >
+      <q-scroll-area class="fit">
+        <q-list padding class="text-grey-8">
+          <q-item class="GNL__drawer-item" v-ripple v-for="link in links1" :key="link.text" clickable>
+            <q-item-section avatar>
+              <q-icon :name="link.icon"/>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ link.text }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data() {
+    return {
+      leftDrawerOpen: false,
+      links1: [
+        {icon: 'web', text: '项目管理'},
+        {icon: 'person', text: '问题追踪'},
+      ],
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="sass">
+.GNL
+  &__toolbar
+    height: 64px
+
+  &__toolbar-input
+    width: 55%
+
+  &__drawer-item
+    line-height: 24px
+    border-radius: 0 24px 24px 0
+    margin-right: 12px
+
+    .q-item__section--avatar
+      .q-icon
+        color: #5f6368
+
+    .q-item__label
+      color: #3c4043
+      letter-spacing: .01785714em
+      font-size: .875rem
+      font-weight: 500
+      line-height: 1.25rem
+
+  &__drawer-footer-link
+    color: inherit
+    text-decoration: none
+    font-weight: 500
+    font-size: .75rem
+
+    &:hover
+      color: #000
+</style>
---
+++ src/pages/Regist.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="q-pa-md" style="max-width: 400px" :class="'fixed-center'">
+    <span CLASS="KKK">项目管理追踪系统注册</span>
+    <q-form action="" class="q-gutter-md">
+      <q-input
+        filled
+        v-model="username"
+        label="Your username *"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+      />
+
+      <q-input
+        v-model="password"
+        label="Your password *"
+        filled
+        :type="isPwd ? 'password' : 'text'"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+
+      <q-input
+        v-model="repassword"
+        label="Enter your password  again*"
+        filled
+        :type="isPwd ? 'password' : 'text'"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+
+      <div>
+        <q-btn
+          label="Submit"
+          @click="onSubmit"
+          color="primary"
+        />
+        <q-btn
+          label="Login"
+          @click="onLogin"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<style lang="sass">
+.KKK
+  position: relative
+  left: 0rem
+  top: -3rem
+  text-align: center
+  width: 150px
+  height: 55px
+  color: #ffffff
+  background-color: #1976d2
+  border-radius: 10px 10px 10px 10px
+  box-shadow: 0 1px 5px rgb(0 0 0 / 20%), 0 2px 2px rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%)
+  font-weight: 500
+  font-size: 1.5rem
+  line-height: 2rem
+  padding: 4px 16px
+  margin: 4rem 0 1.5rem
+</style>
+
+<script>
+import {useQuasar} from "quasar";
+import {ref} from "vue";
+import {api} from "../boot/axios";
+import {useRoute, useRouter} from "vue-router";
+
+export default {
+  setup() {
+    const $q = useQuasar();
+    const username = ref(null);
+    const password = ref(null);
+    const repassword = ref(null);
+    const isPwd = ref(true);
+    const router = useRouter();
+
+    return {
+      username,
+      password,
+      repassword,
+      isPwd,
+      router,
+
+      onSubmit() {
+        api
+          .post("/user/login", {
+            username,
+            password,
+          })
+          .then((successResponse) => {
+            const responseResult = JSON.stringify(successResponse.data);
+
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            if (successResponse.data.code === 200) {
+
+              void router.replace({path: "/login"});
+              alert("注册成功!");
+            }
+          })
+          .catch((failResponse) => {
+            path: "/user/login", alert("登录失败！");
+          });
+      },
+      onLogin() {
+        void router.push({path: '/login'})
+      },
+    }
+  },
+};
+</script>
+
+<style scoped></style>
