e29a644f2854f5e9841b5aa4cf6a613d582365b8
Sparrow <704998200@qq.com>
2021-05-06 14:32:40 +0800

    666

---
+++ src/components/newproject.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-dialog v-model="layout">
+    <q-layout view="Lhh lpR fff" container class="bg-white">
+      <q-header class="bg-primary">
+      </q-header>
+      <q-page-container>
+        <q-page padding>
+          <q-form
+            class="q-gutter-md"
+          >
+            <q-input
+              outlined
+              v-model="projectname"
+              label="项目名称 *"
+              hint="项目名称"
+            />
+            <q-select outlined multiple v-model="multiple" :options="options" label="项目人员"/>
+
+            <div>
+              <q-btn label="Submit" type="submit" color="primary"/>
+            </div>
+          </q-form>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: "newproject",
+  data() {
+    return {}
+  }
+}
+</script>
