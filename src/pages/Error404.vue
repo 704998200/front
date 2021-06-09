97455a9c51323fdb06a22ec218240dcb987d6dc1
Augustine <1511884946@qq.com>
2021-06-09 23:33:25 +0800

    add pages

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -3,7 +3,7 @@
     class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
   >
     <div>
-      <div style="font-size: 30vh">404</div>
+      <div style="font-size: 30vh">114514</div>
 
       <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
 
